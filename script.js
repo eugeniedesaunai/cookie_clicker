e80e53bf94341b54baeeb20af9936e69c9588148
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-07-19 09:09:57 +0200

    add

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 let score = 0;
 let multiplicateur = 1;
 let prixMultiplicateur= 50;
+let prixActuell = 0;
+let prixAutoClick = 500;
 
 function updateDOM() { // modification du texte html
     let dom_elements = document.getElementById('multiplier');
     dom_elements.innerText = `multiplier par : ${multiplicateur}` ;
     
     let prix = document.getElementById('prix');
-    prix.innerText = prixMultiplicateur * multiplicateur;
+    prixActuell = prixMultiplicateur * multiplicateur
+    prix.innerText = `prix : ${prixActuell}` ;
 
     let dom_element = document.getElementById('score');
     dom_element.innerText = score;
 }
 
+//bouton autoclick
 function autoClick() {
-    
+    if (score >= prixAutoClick) {
+        setInterval(() => {
+            score++;
+            updateDOM();
+        }, 1000);
+        score = score - prixAutoClick;
+    }
 }
 
+
+// bouton agmentation
 function augmenterMultiplicateur(){
-    if (score >= 50) {
-        multiplicateur++;
-        score = score - (prixMultiplicateur * multiplicateur - prixMultiplicateur );
-    }
-    updateDOM();
+        let tmpScore = score;
+        if(tmpScore > prixActuell){
+            multiplicateur++;
+            score = score - (prixMultiplicateur * multiplicateur - prixMultiplicateur );
+            
+        }
+        updateDOM();
 }
 
 function augmenterAffichage() {
     score = score + multiplicateur;
     updateDOM();
 }
-
-
